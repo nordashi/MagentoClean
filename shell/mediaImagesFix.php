2ddb332169a1553b88a3156e34ab201603ab090d
splacento-incomm <9a3bsp@gmail.com>
2021-06-09 12:40:21 +0200

    update: refactored do to unnecessary product load
    Removed Mage::getModel('catalog/product')->load($product->getId()) as it was loading again whole product just to load Media Gallery.
    Result: fewer database queries and faster execution

--- shell/mediaImagesFix.php
+++ shell/mediaImagesFix.php
@@ -1,5 +1,4 @@
-<?php 
-// Original from: https://stackoverflow.com/questions/19153891/magento-how-to-programatically-set-the-base-image-to-the-first-image-in-the-lis
+<?php
 ini_set('display_errors','On');
 ini_set('memory_limit','512M');
 error_reporting(E_ALL);
@@ -10,20 +9,23 @@ class Mage_Shell_Updater extends Mage_Shell_Abstract
     public function run()
     {
         $products = Mage::getResourceModel('catalog/product_collection');
-        foreach($products as $p) {
-            $pid = $p->getId();
-            $product = Mage::getModel('catalog/product')->load($pid);
+        foreach($products as $product) {
+            $attribute = $product->getResource()->getAttribute('media_gallery'); //get attribute
+            $backend = $attribute->getBackend();
+            $backend->afterLoad($product);
+            
             $mediaGallery = $product->getMediaGallery();
-            if (isset($mediaGallery['images'])){
+            if (isset($mediaGallery['images']) ){
+                echo "Setting {$product->getSku()} gallery. \n";
                 foreach ($mediaGallery['images'] as $image){
                     Mage::getSingleton('catalog/product_action')
-                    ->updateAttributes(array($pid), array('image'=>$image['file']), 0);
+				            ->updateAttributes(array($product->getId()), array('image' => $image['file'], 'thumbnail' => $image['file'], 'small_image' => $image['file'] ), 0);
                     $this->count++;
                     break;
                 }
             }
         }
-        echo("{$this->count} product(s) updated.");
+        echo "{$this->count} product(s) updated.";
     }
 }
 
